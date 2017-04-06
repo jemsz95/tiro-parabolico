f5be7abda2a0ce9ffbac3acea0707b068c434855
Jesus Guadiana <jisus130@hotmail.com>
2017-04-05 21:40:29 -0500

    1.Se agrego a la pantalla inicial 2 textviews (Email,Password) con sus correspondientes editTexts para ingresar a la aplicacion junto con el boton de inicio de sesion.2. Se cambio el flujo de las actividades para ya no utilizar Menuactiviy se dirige directamente a Instruccionesactivity al hacer inicio de sesion. 3. Se agrego boton de skip en Instruccionesactivity para ingresar al simulador. 4. Se agrego menu en Simuladoractivity con las opciones de Instrucciones, Creditos y Logout(FALTA IMPLEMENTAR CORRECTAMENTE LOGOUT)

--- app/src/main/java/mx/itesm/tiroparabolico/SimuladoActivity.java
+++ app/src/main/java/mx/itesm/tiroparabolico/SimuladoActivity.java
@@ -1,7 +1,10 @@
 package mx.itesm.tiroparabolico;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SimuladoActivity extends AppCompatActivity {
 
@@ -9,5 +12,39 @@ public class SimuladoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulado);
+
+
+
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_simulador, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.creditos_action) {
+            Intent i = new Intent(this, CreditosActivity.class);
+            startActivity(i);
+        }
+        if (id == R.id.instrucciones_action){
+            Intent i = new Intent(this, InstruccionesActivity.class);
+            startActivity(i);
+        }
+        if(id == R.id.logout_action){
+            finish();
+            //TODO: LOGOUT USER
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
