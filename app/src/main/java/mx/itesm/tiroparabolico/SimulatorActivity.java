4497dbf1dad7f1653c81c9658ba79b111d444bc6
Javier Meza <javenmeza@gmail.com>
2017-04-18 14:04:09 -0500

    Restore menu in simulator activity

--- app/src/main/java/mx/itesm/tiroparabolico/SimulatorActivity.java
+++ app/src/main/java/mx/itesm/tiroparabolico/SimulatorActivity.java
@@ -1,7 +1,10 @@
 package mx.itesm.tiroparabolico;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SimulatorActivity extends AppCompatActivity implements DatosFragment.OnGraphDataChangeListener {
 
@@ -9,6 +12,43 @@ public class SimulatorActivity extends AppCompatActivity implements DatosFragmen
     DatosFragment datosFragment;
 
     boolean isLandscape;
+
+    @Override
+    public void onGraphDataChange(double angle, double speed) {
+        graphFragment.setTheta(angle);
+        graphFragment.setV0(speed);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_simulador, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.creditos_action) {
+            Intent i = new Intent(this, CreditosActivity.class);
+            startActivity(i);
+        }
+        if (id == R.id.instrucciones_action){
+            Intent i = new Intent(this, InstruccionesActivity.class);
+            startActivity(i);
+        }
+        if(id == R.id.logout_action){
+            finish();
+            //TODO: LOGOUT USER
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +61,5 @@ public class SimulatorActivity extends AppCompatActivity implements DatosFragmen
                 .findFragmentById(R.id.fragment_data);
 
         isLandscape = datosFragment == null;
-    }
-
-    @Override
-    public void onGraphDataChange(double angle, double speed) {
-        graphFragment.setTheta(angle);
-        graphFragment.setV0(speed);
     }
 }
