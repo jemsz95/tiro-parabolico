f626c351dea9e247bf6754d48e9ce5b033310362
Jesus Guadiana <jisus130@hotmail.com>
2017-04-23 15:58:04 -0500

    Validaciones de login y de los datos del simulador

--- app/src/main/java/mx/itesm/tiroparabolico/LoginActivity.java
+++ app/src/main/java/mx/itesm/tiroparabolico/LoginActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import static mx.itesm.tiroparabolico.R.styleable.View;
 
@@ -14,6 +16,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     Button btnLogin;
     Button btnLogin2;
     ImageView btnInfo;
+    EditText etUsuario;
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btnLogin = (Button) findViewById(R.id.button_login);
         btnInfo = (ImageView) findViewById(R.id.button_info);
         btnLogin2=(Button)findViewById(R.id.button_login2);
-
+        etUsuario = (EditText) findViewById(R.id.editText_mail);
+        etPassword = (EditText) findViewById(R.id.editText_password);
+        etUsuario.setText("");
+        etPassword.setText("");
         btnLogin.setOnClickListener(this);
         btnInfo.setOnClickListener(this);
         btnLogin2.setOnClickListener(this);
@@ -32,12 +39,18 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button_login: {
-                Intent i = new Intent(this, InstruccionesActivity.class);
-                i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                startActivity(i);
+            case R.id.button_login:
+                if(etUsuario.getText().toString().trim().length() > 0 && etPassword.getText().toString().trim().length() > 0)
+                {
+                  Intent i = new Intent(this, InstruccionesActivity.class);
+                  i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                  startActivity(i);
+                }
+                else
+                {
+                    Toast.makeText(this, "Ingrese TODOS los datos", Toast.LENGTH_SHORT).show();
+                }
                 break;
-            }
 
             case R.id.button_info: {
                 //TODO: Show app info
@@ -45,9 +58,16 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
 
             case R.id.button_login2: {
-                Intent i = new Intent(this, InstruccionesActivity.class);
-                i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                startActivity(i);
+                if(etUsuario.getText().toString().trim().length() > 0 && etPassword.getText().toString().trim().length() > 0)
+                {
+                    Intent i = new Intent(this, InstruccionesActivity.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    startActivity(i);
+                }
+                else
+                {
+                    Toast.makeText(this, "Ingrese TODOS los datos", Toast.LENGTH_SHORT).show();
+                }
                 break;
             }
         }
