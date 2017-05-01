3ecbc95aa6a6df028686d4c438eff95ef6a25f7f
Jorge Emilio Rubio Barboza <jorge.rubiobarboza96@gmail.com>
2017-05-01 17:08:53 -0500

    cambios en clases y layouts

--- app/src/main/java/mx/itesm/tiroparabolico/RegisterActivity.java
+++ app/src/main/java/mx/itesm/tiroparabolico/RegisterActivity.java
@@ -1,15 +1,11 @@
 package mx.itesm.tiroparabolico;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 /**
  * Created by jorgeemiliorubiobarboza on 23/04/17.
@@ -31,14 +27,11 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
 
-<<<<<<< HEAD
-        tvTitle = (TextView) findViewById(R.id.text_RegisterTitle);
-=======
+
         tvTitle = (TextView) findViewById(R.id.text_title_instructions);
->>>>>>> 13046c9c753f9f9ee6ecb552d1ce5250688b6cd3
         tvName = (TextView) findViewById(R.id.text_NameRegister);
         tvSecondName = (TextView) findViewById(R.id.text_SecondNameRegister);
-        tvPasswordRegister = (TextView) findViewById(R.id.text_PasswordRegist);
+        tvPasswordRegister = (TextView) findViewById(R.id.text_Class);
         etName = (EditText) findViewById(R.id.edit_FirstName);
         etSecondName =(EditText) findViewById(R.id.edit_LastName);
         etPassword = (EditText) findViewById(R.id.edit_PasswordRegister);
