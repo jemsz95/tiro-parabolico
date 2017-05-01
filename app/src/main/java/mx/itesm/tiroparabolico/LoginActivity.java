e4d58cacb2df11f335ea7f0d832af3c763121674
Jesus Guadiana <jisus130@hotmail.com>
2017-05-01 16:42:16 -0500

    Se agrego la funcionalidad de registrar usuarios a firebase y login con los datos registrados en firebase

--- app/src/main/java/mx/itesm/tiroparabolico/LoginActivity.java
+++ app/src/main/java/mx/itesm/tiroparabolico/LoginActivity.java
@@ -1,13 +1,21 @@
 package mx.itesm.tiroparabolico;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,11 +23,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     Button btnRegistro;
     EditText etUsuario;
     EditText etPassword;
+    private FirebaseAuth firebaseAuth;
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        //si el objeto getcurrentuser no es null
+        //significa que el usuario ya esta loggeado
+
+        if(firebaseAuth.getCurrentUser()!=null){
+            //cierra la actividad
+            finish();
+            //abre instricciones activity
+            startActivity(new Intent(getApplicationContext(),InstruccionesActivity.class));
+        }
 
         btnLogin = (Button) findViewById(R.id.button_login);
         btnRegistro=(Button)findViewById(R.id.button_registro);
@@ -27,8 +49,42 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         etPassword = (EditText) findViewById(R.id.editText_password);
         etUsuario.setText("");
         etPassword.setText("");
+        progressDialog=new ProgressDialog(this);
         btnLogin.setOnClickListener(this);
         btnRegistro.setOnClickListener(this);
+    }
+
+    private void userLogin(){
+        String email=etUsuario.getText().toString().trim();
+        String password=etPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Porfavor ingresa tu email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Porfavor ingressa tu password",Toast.LENGTH_LONG).show();
+            return;
+
+    } progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        //if the task is successfull
+                        if(task.isSuccessful()){
+                            //start the profile activity
+                            finish();
+                            Intent intent = new Intent(getApplicationContext(), InstruccionesActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                            startActivity(intent);
+                        }
+                    }
+                });
+
     }
 
     @Override
@@ -40,16 +96,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 break;
 
             case R.id.button_login: {
-                if(etUsuario.getText().toString().trim().length() > 0 && etPassword.getText().toString().trim().length() > 0)
-                {
-                    Intent intent = new Intent(this, InstruccionesActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                    startActivity(intent);
-                }
-                else
-                {
-                    Toast.makeText(this, "Ingrese TODOS los datos", Toast.LENGTH_SHORT).show();
-                }
+              userLogin();
                 break;
             }
         }
