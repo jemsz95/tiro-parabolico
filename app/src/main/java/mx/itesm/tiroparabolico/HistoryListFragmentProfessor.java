3ffe8bc2df15c61f3eac22c2e8437a45196e4f11
Jorge Emilio Rubio Barboza <jorge.rubiobarboza96@gmail.com>
2017-05-03 14:53:06 -0500

    cambios en clases para listview del professor y launch

---
+++ app/src/main/java/mx/itesm/tiroparabolico/HistoryListFragmentProfessor.java
@@ -0,0 +1,104 @@
+package mx.itesm.tiroparabolico;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by jorgeemiliorubiobarboza on 03/05/17.
+ */
+
+public class HistoryListFragmentProfessor extends ListFragment {
+
+    String [] listaNombreLaunch = {"Tiro1", "Tiro2"};
+    ArrayList<Launch> listaLaunch;
+    ArrayAdapter<DatabaseReference> adapterLaunch;
+    HistoryListFragment.OnLaunchSelectedListener launchListener;
+
+
+    public HistoryListFragmentProfessor() {
+
+    }
+
+    public static HistoryListFragmentProfessor newInstance() {
+        HistoryListFragmentProfessor historyListFragmentProfessor = new HistoryListFragmentProfessor();
+        return historyListFragmentProfessor;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Launch launch = dataSnapshot.getValue(Launch.class);
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+
+        DatabaseReference launchesReference = FirebaseDatabase.getInstance().getReference("/launches");
+        launchesReference.addValueEventListener(postListener);
+
+        adapterLaunch = new ArrayAdapter<DatabaseReference>(getActivity(), android.R.layout.simple_list_item_activated_1,
+                launchesReference);
+        setListAdapter(adapterLaunch);
+
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if(context instanceof HistoryListFragment.OnLaunchSelectedListener){
+            launchListener = (HistoryListFragment.OnLaunchSelectedListener) context;
+        } else {
+            throw new ClassCastException(context.toString()
+                    + "must implement HistoryListFragment.OnItemSelectedListener");
+        }
+    }
+
+
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        double angle = 20;
+        double speed = 15;
+        double height = 2;
+
+        Launch launch = new Launch();
+
+        launch.setV0(speed);
+        launch.setTheta(angle);
+        launch.setY0(height);
+        launch.calculate();
+
+        launchListener.onLaunchSelected(launch);
+    }
+
+    public interface OnLaunchSelectedListener {
+        public void onLaunchSelected(Launch l);
+    }
+}
+
