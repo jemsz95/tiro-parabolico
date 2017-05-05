352630fdf477de37cd44080af7b69aec51bfa6bd
Jorge Emilio Rubio Barboza <jorge.rubiobarboza96@gmail.com>
2017-05-05 16:30:21 -0500

    se modifico el adaptador del historial de vista para poder crear la vista del profesor, al igual que se creo un nuevo adaptador para poder obtener los datos del servidos

--- app/src/main/java/mx/itesm/tiroparabolico/HistoryListFragment.java
+++ app/src/main/java/mx/itesm/tiroparabolico/HistoryListFragment.java
@@ -9,7 +9,15 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
 
 
 public class HistoryListFragment extends ListFragment {
@@ -17,6 +25,7 @@ public class HistoryListFragment extends ListFragment {
     String [] listaNombreLaunch = {"Tiro1", "Tiro2"};
     ArrayList<Launch> listaLaunch;
     ArrayAdapter<String> adapterLaunch;
+    StudentAdapterLaunch adapterLaunch2;
     OnLaunchSelectedListener launchListener;
 
     public HistoryListFragment() {
@@ -31,8 +40,27 @@ public class HistoryListFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        adapterLaunch = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1,
-                listaNombreLaunch);
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Launch object and use the values to update the UI
+                Launch launch = dataSnapshot.getValue(Launch.class);
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Launch failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        DatabaseReference classMemeberRef = FirebaseDatabase.getInstance().getReference("/class_member/1234" );
+        DatabaseReference launchesReference = FirebaseDatabase.getInstance().getReference("/launches");
+        launchesReference.addValueEventListener(postListener);
+        adapterLaunch2 = new StudentAdapterLaunch(getActivity(), android.R.layout.simple_list_item_activated_1,classMemeberRef,
+                launchesReference);
         setListAdapter(adapterLaunch);
     }
 
