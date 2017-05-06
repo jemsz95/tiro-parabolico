6a92f04600e196772009c8d86ae9417a1f552bc1
Jorge Emilio Rubio Barboza <jorge.rubiobarboza96@gmail.com>
2017-05-06 16:48:12 -0500

    creacion de la vista para el historial row.xml

--- app/src/main/java/mx/itesm/tiroparabolico/StudentAdapterLaunch.java
+++ app/src/main/java/mx/itesm/tiroparabolico/StudentAdapterLaunch.java
@@ -11,6 +11,13 @@ import com.google.firebase.database.Query;
  * Created by jorgeemiliorubiobarboza on 03/05/17.
  */
 
+
+/*
+*
+*   clase que apoya para adquirir todos los launch's de un usuario de firebase
+*   mediante firebase  IndexListAdapter
+*   */
+
 public class StudentAdapterLaunch extends FirebaseIndexListAdapter<Launch> {
 
     public StudentAdapterLaunch(Activity actual, @LayoutRes int modelLayout, Query keyRef, Query dataRef){
