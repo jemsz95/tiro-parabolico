de311d0b9afd74bced65606343fa921263f107c6
Javier Meza <javenmeza@gmail.com>
2017-05-06 20:35:54 -0500

    Adds favorite button

--- app/src/main/java/mx/itesm/tiroparabolico/HistoryListFragment.java
+++ app/src/main/java/mx/itesm/tiroparabolico/HistoryListFragment.java
@@ -26,10 +26,7 @@ import static android.content.ContentValues.TAG;
 
 public class HistoryListFragment extends ListFragment {
 
-    String [] listaNombreLaunch = {"Tiro1", "Tiro2"};
-    ArrayList<Launch> listaLaunch;
-    ArrayAdapter<String> adapterLaunch;
-    StudentAdapterLaunch adapterLaunch2;
+    StudentAdapterLaunch adapterLaunch;
     OnLaunchSelectedListener launchListener;
 
     public HistoryListFragment() {
@@ -45,29 +42,11 @@ public class HistoryListFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ValueEventListener postListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // Get Launch object and use the values to update the UI
-                Launch launch = dataSnapshot.getValue(Launch.class);
-                // ...
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // Getting Launch failed, log a message
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
-                // ...
-            }
-        };
-
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference classMemeberRef = Database.getInstance().getReference("/class_member/1234" );
         Query launchesReference = Database.getInstance().getReference("/launches").orderByChild("author").equalTo(user.getUid());
 
-        launchesReference.addValueEventListener(postListener);
+        adapterLaunch = new StudentAdapterLaunch(getActivity(), R.layout.row, launchesReference);
 
-        adapterLaunch2 = new StudentAdapterLaunch(getActivity(), android.R.layout.simple_list_item_activated_1, launchesReference);
         setListAdapter(adapterLaunch);
     }
 
@@ -77,35 +56,13 @@ public class HistoryListFragment extends ListFragment {
         if(context instanceof OnLaunchSelectedListener){
             launchListener = (OnLaunchSelectedListener) context;
         } else {
-            throw new ClassCastException(context.toString()
-                    + "must implement HistoryListFragment.OnItemSelectedListener");
+            throw new ClassCastException(context.toString() + "must implement HistoryListFragment.OnItemSelectedListener");
         }
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        double angle;
-        double speed;
-        double height;
-
-        if (position == 0) {
-            angle = 20;
-            speed = 15;
-            height = 2;
-        }
-        else {
-            angle = 10;
-            speed = 20;
-            height = 0;
-        }
-
-        Launch launch = new Launch();
-
-        launch.setV0(speed);
-        launch.setTheta(angle);
-        launch.setY0(height);
-        launch.calculate();
-
+        Launch launch = adapterLaunch.getItem(position);
         launchListener.onLaunchSelected(launch);
     }
 
