1b3b844fe2b94d513df1266581f25014d6e3266f
Javier Meza <javenmeza@gmail.com>
2017-05-07 14:20:08 -0500

    Change menu in history activity

--- app/src/main/java/mx/itesm/tiroparabolico/HistorialActivity.java
+++ app/src/main/java/mx/itesm/tiroparabolico/HistorialActivity.java
@@ -39,7 +39,7 @@ public class HistorialActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_simulador, menu);
+        getMenuInflater().inflate(R.menu.menu_filtro, menu);
         return true;
     }
 
