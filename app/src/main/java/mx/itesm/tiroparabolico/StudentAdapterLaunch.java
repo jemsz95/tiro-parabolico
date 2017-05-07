87ea8a57f97989971d7e171252d56426844c756a
Javier Meza <javenmeza@gmail.com>
2017-05-06 20:35:54 -0500

    Adds favorite button

--- app/src/main/java/mx/itesm/tiroparabolico/StudentAdapterLaunch.java
+++ app/src/main/java/mx/itesm/tiroparabolico/StudentAdapterLaunch.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.renderscript.Double2;
 import android.support.annotation.LayoutRes;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseIndexListAdapter;
@@ -14,7 +15,6 @@ import com.google.firebase.database.Query;
  * Created by jorgeemiliorubiobarboza on 03/05/17.
  */
 /*
-*
 *   clase que apoya para adquirir todos los launch's de un usuario de firebase
 *   mediante firebase  IndexListAdapter
 *   */
@@ -27,16 +27,27 @@ public class StudentAdapterLaunch extends FirebaseListAdapter<Launch> {
     }
 
     @Override
-    protected void populateView(View v, Launch l, int position ){
+    protected void populateView(View v, final Launch l, int position ){
         TextView tvStudent = (TextView) v.findViewById(R.id.text_studentHistory);
         TextView tvAngle = (TextView) v.findViewById(R.id.text_angleHistory);
         TextView tvVelocity = (TextView) v.findViewById(R.id.text_velocityHistory);
         TextView tvHeight = (TextView) v.findViewById(R.id.text_heightHistory);
+        ImageButton ibFavorite = (ImageButton) v.findViewById(R.id.button_favorite);
 
         tvStudent.setText(l.getUserName());
         tvAngle.setText(Double.toString(l.getTheta()));
         tvVelocity.setText(Double.toString(l.getV0()));
         tvHeight.setText(Double.toString(l.getY0()));
+        ibFavorite.setImageResource(l.isFavorite() ? R.drawable.ic_star_24dp : R.drawable.ic_star_border_24dp);
+
+        ibFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Database.getInstance().getReference()
+                        .child("tiros/" + l.getId() + "/favorite")
+                        .setValue(!l.isFavorite());
+            }
+        });
     }
 }
 
