c9a2ca0f52e4d000f9c3d5c8d021cb2385269f06
Javier Meza <javenmeza@gmail.com>
2017-05-07 20:28:23 -0500

    Fix historyactivity

--- app/src/main/java/mx/itesm/tiroparabolico/HistoryListFragment.java
+++ app/src/main/java/mx/itesm/tiroparabolico/HistoryListFragment.java
@@ -2,11 +2,8 @@ package mx.itesm.tiroparabolico;
 
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.support.v4.content.res.ResourcesCompat;
 import android.view.View;
 import android.widget.ListView;
 
@@ -14,15 +11,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.Query;
 
-import java.util.HashSet;
-import java.util.Set;
-
-
 public class HistoryListFragment extends ListFragment {
 
     StudentAdapterLaunch adapterLaunch;
     OnLaunchSelectedListener launchListener;
-    Set<Integer> selectedPos;
 
     public HistoryListFragment() {
 
@@ -40,8 +32,6 @@ public class HistoryListFragment extends ListFragment {
         Query launchesReference = Database.getInstance().getReference("/launches").orderByChild("author").equalTo(user.getUid());
 
         adapterLaunch = new StudentAdapterLaunch(getActivity(), R.layout.row, launchesReference);
-        selectedPos = new HashSet<>();
-
         setListAdapter(adapterLaunch);
     }
 
@@ -59,16 +49,6 @@ public class HistoryListFragment extends ListFragment {
     public void onListItemClick(ListView l, View v, int position, long id) {
         Launch launch = adapterLaunch.getItem(position);
         launchListener.onLaunchSelected(launch);
-
-        if(selectedPos.contains(position)) {
-            int c = ResourcesCompat.getColor(getResources(), R.color.list_default_color, null);
-            v.setBackgroundColor(c);
-            selectedPos.remove(position);
-        } else {
-            int c = ResourcesCompat.getColor(getResources(), R.color.list_pressed_color, null);
-            v.setBackgroundColor(c);
-            selectedPos.add(position);
-        }
     }
 
     public interface OnLaunchSelectedListener {
